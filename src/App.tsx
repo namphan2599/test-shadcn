d212d0dba2afdac26ed226896c7ca018adb47d64
Phan Nam <43086125+namphan2599@users.noreply.github.com>
2024-03-04 17:06:17 +0700

    add form

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,96 @@
-import { Button } from './components/ui/button'
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { json } from "stream/consumers";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  email: z.string().email({ message: "Nhap email zo" }).min(5),
+});
 
 function App() {
-
   return (
     <>
-      <Button variant="secondary">Yeah</Button>
+      <ProfileForm />
     </>
-  )
+  );
 }
 
-export default App
+export function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      email: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    const rs = await fetch("http://localhost:3000/test", {
+      headers: { "Content-Type": "application/json" },
+      method: "post",
+      body: JSON.stringify(values),
+    });
+
+    console.log(await rs.json());
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="email" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default App;
